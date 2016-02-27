82da9216419e0e8fe4d265fb1d0703767a5ba5d9
atoz-programming-tutorials <atozprogrammingtutorials@gmail.com>
2016-02-27 00:49:21 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,53 @@
+// Lesson 1
+// Open a SDL window and fill the background with red
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+
+static const int width = 800;
+static const int height = 600;
+
+int main(int argc, char **argv)
+{
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create a SDL window
+    SDL_Window *window = SDL_CreateWindow("Hello, SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+
+    // Create a renderer (accelerated and in sync with the display refresh rate)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    bool running = true;
+    SDL_Event event;
+    while(running)
+    {
+        // Process events
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        // Clear screen
+        SDL_RenderClear(renderer);
+
+        // Draw
+
+        // Show what was drawn
+        SDL_RenderPresent(renderer);
+    }
+
+    // Release resources
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
